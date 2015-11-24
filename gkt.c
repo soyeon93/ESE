e71d77c5be26210b459b5f2270e779b6e9207af6
soyeon93 <csycherry93@gmail.com>
2015-11-24 16:45:11 +0900

    Create gkt.c

---
+++ gkt.c
@@ -0,0 +1,19 @@
+#include<gtk/gtk.h>
+
+GtkWidget* window;
+GtkWidget* label1;
+GtkWidget* vbox;
+GtkWidget* btn4_5,*btn4,*btn3_5,*btn3,*btn2_5,*btn2,*btn1_5, *btn1;
+int flag=0;
+int main(int argc, char *argv[])
+{
+
+	int result =0;
+
+ if(flag == 0)
+{
+	//c
+}	
+
+
+}
